cbb3415a5b641b456155b4b20ac139ad6288415d
dvm-stack <61616525+dvm-stack@users.noreply.github.com>
2020-02-29 06:29:08 -0300

    Add files via upload

--- lang-pt.php
+++ lang-pt.php
@@ -5,3 +5,7 @@
 Sistema Multi Linguagem PHP
 ***  Linguagem : Português - PT-BR   ***
 */
+
+define('TITULO','Este é o título');
+define('SUBTITULO','Este é o sub-título');
+define('SUBTITULO2','Este é o sub-título 2');
