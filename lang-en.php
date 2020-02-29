dcd9c69dd9eed1b9105b26a29c11f323eb4bd67e
Diego <attbind@gmail.com>
2020-02-29 06:40:10 -0300

    Send file eng-us

---
+++ lang-en.php
@@ -0,0 +1,11 @@
+<?
+
+
+/*
+Sistema Multi Linguagem PHP
+***  Linguagem : Inglês - ENG-US   ***
+*/
+
+define('TITULO','This is the title');
+define('SUBTITULO','This is the sub-title');
+define('SUBTITULO2','This is the sub-title 2');
