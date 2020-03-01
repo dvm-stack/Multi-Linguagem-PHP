bdf49e2a9f846c53b16b563e525781ea0e5570b2
DVM <61616525+dvm-stack@users.noreply.github.com>
2020-03-01 03:11:30 -0300

    Update

--- lang-pt.php
+++ lang-pt.php
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
+
 <?
+
+
 
 
 /*
@@ -9,16 +11,4 @@ Sistema Multi Linguagem PHP
 
 define('TITULO','Este é o título');
 define('SUBTITULO','Este é o sub-título');
-=======
-<?
-
-
-/*
-Sistema Multi Linguagem PHP
-***  Linguagem : Português - PT-BR   ***
-*/
-
-define('TITULO','Este é o título');
-define('SUBTITULO','Este é o sub-título');
->>>>>>> cbb3415a5b641b456155b4b20ac139ad6288415d
 define('SUBTITULO2','Este é o sub-título 2');
