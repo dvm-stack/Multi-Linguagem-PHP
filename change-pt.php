ba59bd13cb87ba7769c67ba863a8072e12af3466
DVM <61616525+dvm-stack@users.noreply.github.com>
2020-03-05 23:32:01 -0300

    Update change-pt.php

--- change-pt.php
+++ change-pt.php
@@ -6,7 +6,7 @@ Sistema Multi Linguagem PHP
 ***   Não esqueça de definir um tempo para expirar a sessão (de preferência todas) no seu include/require inicial, onde você inicia as sessões (session_start) e etc... ***
 */
 
-<?php 
+session_start();
 $_SESSION["lang"]=="PTBR";
 $back = $_SERVER['HTTP_REFERER'];
 header('Location: ' . $back);
