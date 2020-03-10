d0c4ee015d6667a1751fa79b6b639ba8e6ff5492
DVM <61616525+dvm-stack@users.noreply.github.com>
2020-03-10 03:21:31 -0300

    Update change-en.php

--- change-en.php
+++ change-en.php
@@ -3,7 +3,9 @@
 Sistema Multi Linguagem PHP
 ***  Preferível chamar este arquivo com um "require_once"   ***
 ***  Trocando a linguagem   ***
-***   Não esqueça de definir um tempo para expirar a sessão (de preferência todas) no seu include/require inicial, onde você inicia as sessões (session_start) e etc... ***
+***  Não esqueça de definir um tempo para expirar a sessão (de preferência todas) 
+***  no seu include/require inicial, onde você inicia as sessões (session_start) e etc... 
+***
 */
 
 session_start();
